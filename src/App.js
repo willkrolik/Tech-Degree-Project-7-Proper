66a20a99345fd11c07035d5e8dad8a1f402f5af0
William Krolik <wkrolik@InComm.com>
2019-08-28 16:05:22 -0700

    works with wrong API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import SearchForm from './Components/SearchForm';
+import GifList from './Components/GifList';
+import Buttons from './Components/Buttons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      gifs: [],
+      loading: true
+    };
+  } 
+
+  componentDidMount() {
+    this.performSearch();
+  }
+  
+  performSearch = (query = 'cats') => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
+      .then(response => {
+        this.setState({
+          gifs: response.data.data,
+          loading: false
+        });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });    
+  }
+  
+  render() { 
+    console.log(this.state.gifs);
+    return (
+      <div>
+        <div className="main-header">
+          <div className="inner">
+            <h1 className="main-title">GifSearch</h1>
+            
+            <SearchForm onSearch={this.performSearch} />    
+            <Buttons />
+          </div>   
+        </div>    
+        
+        <div className="photo-container"> 
+          {
+            (this.state.loading)
+             ? <p>Loading...</p>
+             : <GifList data={this.state.gifs} />
+          }  
+          </div>        
+        </div>
+      
+    );
+  }
 }
-
-export default App;
