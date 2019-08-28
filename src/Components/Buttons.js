ac742f62eec08d1f9de31a56112b2922753ec099
William Krolik <wkrolik@InComm.com>
2019-08-28 16:11:08 -0700

    works with wrong api

---
+++ src/Components/Buttons.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Buttons = props => (
+    <ul className="course-nav">
+    <li><a href='/courses/Cats'>Cats</a></li>
+    <li><a href='/courses/Dogs'>Dogs</a></li>
+    <li><a href='/courses/Birds'>Birds</a></li>
+  </ul> 
+);
+
+export default Buttons;
---
+++ src/Components/config.js
@@ -0,0 +1,3 @@
+const apiKey = '120f3ea66ea5e0d848bd2fc27ea62df8';
+
+export default apiKey;
